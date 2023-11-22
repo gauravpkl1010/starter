da84d181fcb843838bf1c679b48e8d1774c4adbf
gauravpkl1010 <Pyakurelgaurav01@gmail.com>
2023-11-22 14:17:19 +0545

    added

--- components/Socials.js
+++ components/Socials.js
@@ -5,19 +5,19 @@ const Socials = () => {
   return (
     <div className="flex items-center gap-x-5 text-lg">
       <Link
-        href={"/"}
+        href={"https://www.facebook.com/profile.php?id=100016364044268"}
         className="hover:text-accent hover:scale-150 transition-all duration-300"
       >
         <RiFacebookLine />
       </Link>
       <Link
-        href={"/"}
+        href={"https://instagram.com/pkl_gaurav?igshid=OGQ5ZDc2ODk2ZA=="}
         className="hover:text-accent hover:scale-150 transition-all duration-300"
       >
         <RiInstagramLine />
       </Link>
       <Link
-        href={"/"}
+        href={"https://github.com/gauravpkl1010"}
         className="hover:text-accent hover:scale-150 transition-all duration-300"
       >
         <RiGithubLine />
